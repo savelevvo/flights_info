a2b70483aa3ab2dbf4b6fbaaeb5d6f27fdddd997
viktor <tugotron45@gmail.com>
2019-08-13 23:26:06 +0300

    Started implementing xml parser;

--- parser.py
+++ parser.py
@@ -1,3 +1,13 @@
+import xml.etree.ElementTree as ET
+
 
 def parse_data():
-    return dict()
+    tree = ET.parse('xml/RS_Via-3.xml')
+    root = tree.getroot()
+
+    flights = root.find('PricedItineraries')
+    for _id, flight in enumerate(flights):
+        _onward = flight.findall("./OnwardPricedItinerary//Flight")
+        _return = flight.findall("./ReturnPricedItinerary//Flight")
+        # _all_flights = flight.findall(".//Flight")
+        total_price = float(flight.find("./Pricing//*[@ChargeType='TotalAmount']").text)
