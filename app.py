ca377868fdb23450c15ddce0b2c6b5fa9653b06f
viktor <tugotron45@gmail.com>
2019-08-16 00:13:42 +0300

    Refactoring;

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, abort
-from parser import parse_data
 from functools import lru_cache
+from parser import parse_data
 
 app = Flask('flights_info')
 
@@ -10,74 +10,76 @@ def get_data():
     return parse_data()
 
 
-def get_flights(route, route2=None, key=None) -> list:
-    full_flights_info, for_search = get_data()
-    all_flights_ids = for_search[route]['ids']
+def handler(route, route2=None, key=None) -> list:
+    full_info, summary_info = get_data()
+    all_flights_ids = summary_info[route]['ids']
 
     if key:
         if key == 'optimal':
-            flight_id = for_search[route]['optimal_id']
+            flight_id = summary_info[route]['optimal_id']
         else:
-            flight_id = for_search[route][key]['id']
-        return full_flights_info[flight_id]
+            flight_id = summary_info[route][key]['id']
+        return full_info[flight_id]
 
     if route2:
-        lhs, rhs = for_search[route], for_search[route2]
+        lhs, rhs = summary_info[route], summary_info[route2]
         return [{
-            'more_flights': abs(len(lhs['ids'])-len(rhs['ids'])),
-            'min_price_diff': round(abs(lhs['min_price']['val']-rhs['min_price']['val']), 2),
-            'max_price_diff': round(abs(lhs['max_price']['val']-rhs['max_price']['val']), 2),
-            'min_length_diff': round(abs(lhs['min_length']['val']-rhs['min_length']['val']), 2),
-            'max_length_diff': round(abs(lhs['max_length']['val']-rhs['max_length']['val']), 2)
+            'more_flights': abs(len(lhs['ids']) - len(rhs['ids'])),
+            'min_price_diff': round(abs(lhs['min_price']['val'] - rhs['min_price']['val']), 2),
+            'max_price_diff': round(abs(lhs['max_price']['val'] - rhs['max_price']['val']), 2),
+            'min_length_diff': round(abs(lhs['min_length']['val'] - rhs['min_length']['val']), 2),
+            'max_length_diff': round(abs(lhs['max_length']['val'] - rhs['max_length']['val']), 2)
         }]
 
     else:
-        all_flights_info = [full_flights_info[f] for f in all_flights_ids if f in all_flights_ids]
+        all_flights_info = [full_info[f] for f in all_flights_ids if f in all_flights_ids]
         return all_flights_info
 
 
-@app.route('/get-airports', methods=['GET'])
-def get_airports():
+@app.route('/flights', methods=['GET'])
+def flights():
     """
-    :param str route: Source and destination airports code: SRC-DST
-    :return: list of all flights from -> to
+    :param str route: Source and destination airports code. Format: SRC-DST
+    :return: all flights from -> to
     """
+
     if request.method == 'GET':
-        route = request.args.get('route')
-        if not route:
+        _route = request.args.get('route')
+        if not _route:
             abort(400)
-        result = get_flights(route)
+        result = handler(_route)
         return jsonify(result)
 
 
-@app.route('/get-top', methods=['GET'])
-def get_top():
+@app.route('/top', methods=['GET'])
+def top():
     """
-    :param str route: Source and destination airports code: SRC-DST
-    :param str key: Sorting key. Possible values: min_price, max_price,
-                                                min_length, max_length,
-                                                optimal
-    :return: top flight info
+    :param str route: Source and destination airports code. Format: SRC-DST
+    :param str key: Sorting key. Possible values: min_price, max_price, min_length, max_length, optimal
+    :return: flight info
     """
+
     if request.method == 'GET':
-        route, _key = request.args.get('route'), request.args.get('key')
-        if not all((route, _key)):
+        _route, _key = request.args.get('route'), request.args.get('key')
+        if not all((_route, _key)):
             abort(400)
-        result = get_flights(route, key=_key)
+        result = handler(_route, key=_key)
         return jsonify(result)
 
 
 @app.route('/compare', methods=['GET'])
 def compare():
     """
-
-    :return:
+    :param str route: Source and destination airports code. Format: SRC-DST
+    :param str route2: Source and destination airports code. Format: SRC-DST
+    :return: list of summary diffs
     """
+
     if request.method == 'GET':
-        route, route2 = request.args.get('route'), request.args.get('route2')
-        if not all((route, route2)):
+        _route, _route2 = request.args.get('route'), request.args.get('route2')
+        if not all((_route, _route2)):
             abort(400)
-        result = get_flights(route, route2)
+        result = handler(_route, _route2)
         return jsonify(result)
 
 
--- parser.py
+++ parser.py
@@ -1,56 +1,37 @@
 import xml.etree.ElementTree as ET
 from datetime import datetime
+from collections import defaultdict
 
 
 def parse_data() -> tuple:
     """
     Parses xml file and stores flights info in two hash tables:
-    full_flights_info: contains full description of a flight
-    for_search: optimized for searching min/max values
+    full_info: hash table containing full description of a flight
+    summary_info: hash table for searching min/max values
     :return: tuple of above tables
     """
 
     tree = ET.parse('xml/RS_Via-3.xml')
     root = tree.getroot()
     flights = root.find('PricedItineraries')
-    full_flights_info, for_search = dict(), dict()
-    _prices, _times = dict(), dict()
-    optimals = dict()
+
+    full_info, summary_info = dict(), dict()
+    _prices, _times, _optimals = dict(), dict(), dict()
 
     for _id, flight in enumerate(flights):
         onward_flights = flight.findall("./OnwardPricedItinerary//Flight")
         return_flights = flight.findall("./ReturnPricedItinerary//Flight")
-        # flight_segments = flight.findall(".//Flight")
-
-        total_price = float(flight.find("./Pricing//*[@ChargeType='TotalAmount']").text)
 
         onward_segments, return_segments = list(), list()
+        total_price = float(flight.find("./Pricing//*[@ChargeType='TotalAmount']").text)
 
         first_airport = onward_flights[0].find('Source').text
         last_airport = onward_flights[-1].find('Destination').text
 
-        first_length = datetime.strptime(onward_flights[0].find('DepartureTimeStamp').text, "%Y-%m-%dT%H%M")
-        last_length = datetime.strptime(onward_flights[-1].find('ArrivalTimeStamp').text, "%Y-%m-%dT%H%M")
-        total_length = int(((last_length - first_length).total_seconds()) / 60)  # minutes
+        total_length, prev_arrival = defaultdict(int), defaultdict(int)
 
-        for flight_segment in onward_flights:
-            carrier = flight_segment.find('Carrier').text
-            flight_n = int(flight_segment.find('FlightNumber').text)
-            source = flight_segment.find('Source').text
-            destination = flight_segment.find('Destination').text
-            departure = datetime.strptime(flight_segment.find('DepartureTimeStamp').text, "%Y-%m-%dT%H%M")
-            arrival = datetime.strptime(flight_segment.find('ArrivalTimeStamp').text, "%Y-%m-%dT%H%M")
-
-            segment = {'carrier': carrier, 'flight_number': flight_n, 'source': source, 'destination': destination,
-                       'departure': departure, 'arrival': arrival}
-            onward_segments.append(segment)
-
-        if return_flights:
-            first_length = datetime.strptime(return_flights[0].find('DepartureTimeStamp').text, "%Y-%m-%dT%H%M")
-            last_length = datetime.strptime(return_flights[-1].find('ArrivalTimeStamp').text, "%Y-%m-%dT%H%M")
-            total_length += int(((last_length - first_length).total_seconds()) / 60)  # minutes
-
-            for flight_segment in return_flights:
+        for direction, value in {'onward': onward_flights, 'return': return_flights}.items():
+            for flight_segment in value:
                 carrier = flight_segment.find('Carrier').text
                 flight_n = int(flight_segment.find('FlightNumber').text)
                 source = flight_segment.find('Source').text
@@ -58,38 +39,51 @@ def parse_data() -> tuple:
                 departure = datetime.strptime(flight_segment.find('DepartureTimeStamp').text, "%Y-%m-%dT%H%M")
                 arrival = datetime.strptime(flight_segment.find('ArrivalTimeStamp').text, "%Y-%m-%dT%H%M")
 
+                total_length[direction] += int((arrival - departure).total_seconds() / 60)
+                if prev_arrival[direction]:
+                    total_length[direction] += int((departure - prev_arrival[direction]).total_seconds() / 60)
+
+                prev_arrival[direction] = arrival
+
                 segment = {'carrier': carrier, 'flight_number': flight_n, 'source': source, 'destination': destination,
                            'departure': departure, 'arrival': arrival}
-                return_segments.append(segment)
+
+                if direction == 'onward':
+                    onward_segments.append(segment)
+                elif direction == 'return':
+                    return_segments.append(segment)
+
+        total_length = sum(total_length.values())
 
         route = f'{first_airport}-{last_airport}'
         _prices.setdefault(route, {})[_id] = total_price
         _times.setdefault(route, {})[_id] = total_length
 
-        # get all flight ids for current flight
-        for_search.setdefault(route, {}).setdefault('ids', []).append(_id)
+        summary_info.setdefault(route, {}).setdefault('ids', []).append(_id)
+        full_info[_id] = {'onward': onward_segments, 'return': return_segments, 'total_price': total_price}
 
-        full_flights_info[_id] = {'onward': onward_segments, 'return': return_segments, 'total_price': total_price}
-
-    # assert _prices is sorted by price from min to max
+    # assert records in xml are sorted by price from min to max
     # sort time from min to max
-    for k in _times:
-        _times[k] = dict(sorted(_times[k].items(), key=lambda v: v[1]))
+    for _route in _times:
+        _times[_route] = dict(sorted(_times[_route].items(), key=lambda v: v[1]))
 
-    for route_name in _prices:
-        for _id, _ in _prices[route_name].items():
-            index_price = list(_prices[route_name].keys()).index(_id)
-            index_time = list(_times[route_name].keys()).index(_id)
-            optimals.setdefault(route_name, []).append({_id: index_price + index_time})
+    # find optimal id
+    for _route in _prices:
+        for _id, _ in _prices[_route].items():
+            index_price = list(_prices[_route].keys()).index(_id)
+            index_time = list(_times[_route].keys()).index(_id)
+            _optimals.setdefault(_route, []).append({_id: index_price + index_time})
 
-    for k in for_search:
-        min_price, max_price = list(_prices[k].items())[0], list(_prices[k].items())[-1]
-        min_length, max_length = list(_times[k].items())[0], list(_times[k].items())[-1]
-        optimal_id = next(iter(optimals[k][0]))
-        for_search[k].setdefault('min_price', dict(zip(('id', 'val'), min_price)))
-        for_search[k].setdefault('max_price', dict(zip(('id', 'val'), max_price)))
-        for_search[k].setdefault('min_length', dict(zip(('id', 'val'), min_length)))
-        for_search[k].setdefault('max_length', dict(zip(('id', 'val'), max_length)))
-        for_search[k].setdefault('optimal_id', optimal_id)
+    for _route in summary_info:
+        value_map = {
+            'min_price': list(_prices[_route].items())[0],
+            'max_price': list(_prices[_route].items())[-1],
+            'min_length': list(_times[_route].items())[0],
+            'max_length': list(_times[_route].items())[-1]
+        }
+        for k, v in value_map.items():
+            summary_info[_route].setdefault(k, dict(zip(('id', 'val'), v)))
+        optimal_id = next(iter(_optimals[_route][0]))
+        summary_info[_route].setdefault('optimal_id', optimal_id)
 
-    return full_flights_info, for_search
+    return full_info, summary_info
