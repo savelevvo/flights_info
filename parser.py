86af3e3123bf21fd2988e7ff29a4175b8f34a9ce
Viktor Savelev <tugotron45@gmail.com>
2019-08-15 15:58:40 +0300

    Implementing optimal flight search;

--- parser.py
+++ parser.py
@@ -14,6 +14,8 @@ def parse_data() -> tuple:
     root = tree.getroot()
     flights = root.find('PricedItineraries')
     full_flights_info, for_search = dict(), dict()
+    _prices, _times = dict(), dict()
+    optimals = dict()
 
     for _id, flight in enumerate(flights):
         onward_flights = flight.findall("./OnwardPricedItinerary//Flight")
@@ -44,6 +46,8 @@ def parse_data() -> tuple:
             onward_segments.append(segment)
 
         route = f'{first_airport}-{last_airport}'
+        _prices.setdefault(route, {})[_id] = total_price
+        _times.setdefault(route, {})[_id] = total_length
 
         # get all flight ids for current flight
         for_search.setdefault(route, {}).setdefault('ids', []).append(_id)
@@ -62,13 +66,37 @@ def parse_data() -> tuple:
 
         full_flights_info[_id] = {'onward': onward_segments, 'total_price': total_price}
 
+    # assert _prices is sorted by price from min to max
+    # sort time from min to max
+    for k in _times:
+        _times[k] = dict(sorted(_times[k].items(), key=lambda v: v[1]))
+
+    for route_name in _prices:
+        for _id, _ in _prices[route_name].items():
+            index_price = list(_prices[route_name].keys()).index(_id)
+            index_time = list(_times[route_name].keys()).index(_id)
+            optimals.setdefault(route_name, []).append({_id: index_price+index_time})
+
+    for k in for_search:
+        optimal_id = next(iter(optimals[k][0]))
+        for_search[k].setdefault('optimal_id', optimal_id)
+
     print(for_search)
     return full_flights_info, for_search
 
+
 parse_data()
 
-
 """
+    price | time
+1:  800   | 320 
+2:  500   | 180
+3:  600   | 330
+4:  950   | 510
+
+prices = {2: 500, 3: 600, 1: 800, 4: 950}
+times = {2: 180, 1: 320, 3: 330, 4: 510}
+result = {2: 0, 3: 3, 1: 3, 4: 6}  # {id: sum_of_indexes}
 
 for_search = {
     'SVO-JFK': {
